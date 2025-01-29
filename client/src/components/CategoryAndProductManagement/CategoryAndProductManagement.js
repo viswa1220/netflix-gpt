5fee19a3e5fd9a21aa67e04993f5c50f40374000
viswa1220 <viswanathan122000@gmail.com>
2025-01-29 10:39:23 -0500

    added back button

--- client/src/components/CategoryAndProductManagement/CategoryAndProductManagement.js
+++ client/src/components/CategoryAndProductManagement/CategoryAndProductManagement.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,  } from "react";
 import { graphQLCommand } from "../../util";
+import { useNavigate } from 'react-router-dom';
 import CategoryManagement from "../CategoryManagement/CategoryManagement";
 import ProductManagement from "../ProductManagement/ProductManagement";
 
 const CategoryAndProductManagement = () => {
   // State for categories: each category = { _id, name, categoryImage, categoryID }
   const [categories, setCategories] = useState([]);
-
+  const navigate = useNavigate();
   // Fetch categories on mount
   useEffect(() => {
     const fetchCategories = async () => {
@@ -29,8 +30,8 @@ const CategoryAndProductManagement = () => {
           _id: cat.id, // MongoDB ID
           name: cat.name, // Category Name
           categoryImage: cat.categoryImage || "", // Category Image
-          categoryID: cat.categoryID || "", 
-          description:cat.description || ""// Category ID (fallback to empty string if not present)
+          categoryID: cat.categoryID || "",
+          description: cat.description || "", // Category ID (fallback to empty string if not present)
         }));
 
         setCategories(catObjects);
@@ -42,22 +43,31 @@ const CategoryAndProductManagement = () => {
 
     fetchCategories();
   }, []);
+  const handleBack = () => {
+    navigate('/dashboard');
+  };
+  return (    
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 lg:p-8">
+        {/* Left side: Category Management */}
+        <div className="p-4">
+          <CategoryManagement
+            categories={categories}
+            setCategories={setCategories}
+          />
+        </div>
 
-  return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 lg:p-8">
-      {/* Left side: Category Management */}
-      <div className="p-4">
-        <CategoryManagement 
-          categories={categories} 
-          setCategories={setCategories} 
-        />
+        {/* Right side: Product Management */}
+        <div className="p-4">
+          <ProductManagement categories={categories} />
+        </div>
+        <button
+        onClick={handleBack}
+        className="bg-primaryYellow text-primaryBlack py-2 px-4 rounded-md  hover:bg-yellow-600 0"
+      >
+        Back 
+      </button>
       </div>
-
-      {/* Right side: Product Management */}
-      <div className="p-4">
-        <ProductManagement categories={categories} />
-      </div>
-    </div>
+    
   );
 };
 
